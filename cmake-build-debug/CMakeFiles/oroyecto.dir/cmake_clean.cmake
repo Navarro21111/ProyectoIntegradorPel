f2fef76aa49a3eee1b904f08e41035e19f57218d
Navarro2111 <manpastor2@hotmail.com>
2020-01-20 22:27:45 +0100

    Commit con el codigo del proyecto

---
+++ cmake-build-debug/CMakeFiles/oroyecto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oroyecto.dir/main.cpp.obj"
+  "oroyecto.pdb"
+  "oroyecto.exe"
+  "oroyecto.exe.manifest"
+  "liboroyecto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oroyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
